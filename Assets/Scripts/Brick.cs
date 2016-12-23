c16f105a1df801fa9c3326012b747d59f0878cfe
meatwagon <adamemc2@gmail.com>
2016-12-23 02:36:45 -0900

    through lecture 86
    bricks show damage, advances to the next level when cleared.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,10 +5,12 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
+
 	private int timesHit;
 	private LevelManager levelManager;
-
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+	private bool isBreakable; 
 
 
 
@@ -17,6 +19,11 @@ public class Brick : MonoBehaviour {
 
 		// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "breakable");
+		if (isBreakable) {
+			breakableCount++;
+			print (breakableCount);
+		}
 		timesHit = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager> ();
 	
@@ -24,16 +31,34 @@ public class Brick : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (timesHit >= maxHits) {
-			Destroy (gameObject); 
-		}
+
 
 	}
 	void OnCollisionEnter2D(Collision2D coll) {
+
+		if (isBreakable) {
+			HandleHits();
+		}
+	}
+	void HandleHits () {
 		timesHit ++;
+		int maxHits = hitSprites.Length + 1;
 		print ("I've been hit " + timesHit + " times!!");
+		if (timesHit >= maxHits) {
+			breakableCount--;
+			levelManager.BrickDestroyed();
+			Destroy (gameObject); 
+		} else { 
+			LoadSprites();
+		}
 
 	}
-   
+
+	void LoadSprites (){
+		int spriteIndex = timesHit - 1;
+		if (hitSprites [spriteIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		}
+	}
 
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -14,4 +14,9 @@ public class LevelManager : MonoBehaviour {
 	public void LoadNextLevel(){
 		Application.LoadLevel(Application.loadedLevel + 1);
 	}
+	public void BrickDestroyed(){
+		if (Brick.breakableCount <=0){
+			LoadNextLevel();
+		}
+	}
 }
